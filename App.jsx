fdb433540ebebafab94f6efbd39a80b7c72cca0f
remiheinze-sketch <remiheinze@gmail.com>
2025-11-11 19:13:06 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Bienvenue sur OrganiHub 🧭</h1>
+      <p>
+        Ceci est la base de ton application React + Vite.
+        <br />
+        On va bientôt y ajouter les onglets (Notes, Agenda, etc.).
+      </p>
+    </div>
+  )
+}
+
+export default App
